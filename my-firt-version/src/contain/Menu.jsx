209103c9e13d6f0e29b9a6c2e14cc72c283f845b
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2019-12-15 00:45:37 +0700

    creat first class component for menu

--- my-firt-version/src/contain/Menu.jsx
+++ my-firt-version/src/contain/Menu.jsx
@@ -1,2 +1,19 @@
 import React from 'react';
+// import { render } from '@testing-library/react';
 
+class Menu extends React.Component {
+render(){
+    return(
+        <div className='Menu'>
+            <div className='container'>
+                <div className="nav">
+                    <a href="#">Pricing</a>
+                    <a href="#">About</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+}
+
+export default Menu;
